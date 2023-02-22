28e5fd395bdb54911df87fdf42a0849781d72b33
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-02-21 21:17:00 -0300

    analytics import

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 import {
 	CACHE_SIZE_UNLIMITED,
 	clearIndexedDbPersistence,
@@ -18,7 +20,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 
 clearIndexedDbPersistence(db);
