b7a23d9b391da7c5d1929a26e128d27079640343
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-03-12 17:11:58 -0300

    home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,14 @@
 import Hero from "../components/organisms/Hero/Hero";
+import News from "../components/organisms/News/News";
+import Sellers from "../components/organisms/Sellers/Sellers";
 import Layout from "../layout/Layout";
 
 function Home() {
 	return (
 		<Layout>
 			<Hero />
+			<News />
+			<Sellers />
 		</Layout>
 	);
 }
