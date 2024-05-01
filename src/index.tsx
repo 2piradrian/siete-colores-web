be0ffbd05620266cf93d5a4fc63044ea0a24daf1
Adrian Rodriguez <83842205+2piradrian@users.noreply.github.com>
2024-04-30 22:19:01 -0300

    sorting imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import "./style/global.css"
 import { BrowserRouter } from 'react-router-dom';
+import "./style/global.css"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
