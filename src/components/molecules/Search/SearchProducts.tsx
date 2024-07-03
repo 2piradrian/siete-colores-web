810e472a0bfe2308f1d9d4cef4ef2a3ac1807e3e
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-07-03 14:31:57 -0300

    refactor: Update SearchProducts component to use custom hook for categories

--- src/components/molecules/Search/SearchProducts.tsx
+++ src/components/molecules/Search/SearchProducts.tsx
@@ -1,26 +1,29 @@
 import { Dispatch, SetStateAction, useRef } from "react";
 import { Filters } from "../../../types/filters";
+import useCategories from "../../../hooks/useCategories";
 import style from "./style.module.css";
 
 export default function SearchProducts({ setFilters }: { setFilters: Dispatch<SetStateAction<Filters>> }) {
 
-		const name = useRef<any>();
-		const category = useRef<any>();
-		const order = useRef<any>();
+	const { categories } = useCategories();
 
-		const handleChange = () => {
-	    	const formData: Filters = {
-				category: category.current.value,
-				words: name.current.value,
-				sort: order.current.value === "Menor Precio" ? "lowest" : order.current.value === "Mayor Precio" ? "highest" : "default",
-			}
-			 
-	    	setFilters((prevFilters) => ({ ...prevFilters, ...formData }));
-	  	};
+	const name = useRef<any>();
+	const category = useRef<any>();
+	const order = useRef<any>();
 
-		const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-			e.preventDefault();
+	const handleChange = () => {
+		const formData: Filters = {
+			category: category.current.value,
+			words: name.current.value,
+			sort: order.current.value === "Menor Precio" ? "lowest" : order.current.value === "Mayor Precio" ? "highest" : "default",
 		}
+		 
+		setFilters((prevFilters) => ({ ...prevFilters, ...formData }));
+	};
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+	}
 
 
 	return (
@@ -30,28 +33,9 @@ export default function SearchProducts({ setFilters }: { setFilters: Dispatch<Se
 				<div className={style.selector}>
 					<select name="type" className={style.select} ref={category}>
 						<option>Todos</option>
-						<option>Animales</option>
-						<option>Artesanos</option>
-						<option>Bautismo</option>
-						<option>Comunión</option>
-						<option>Contramoldes</option>
-						<option>Deportes</option>
-						<option>Formas</option>
-						<option>Halloween</option>
-						<option>Letras y números</option>
-						<option>Marcos</option>
-						<option>Navidad</option>
-						<option>Pascuas</option>
-						<option>Personajes</option>
-						<option>Plantas</option>
-						<option>Profesiones</option>
-						<option>Scrapers</option>
-						<option>Sellos</option>
-						<option>Rodillos</option>
-						<option>Texturizadores</option>
-						<option>Toppers</option>
-						<option>Transportes</option>
-						<option>Varios</option>
+						{categories.map((category) => (
+							<option key={category}>{category}</option>
+						))}
 					</select>
 					<select name="order" className={style.select} ref={order}>
 						<option>Sin orden</option>
---
+++ src/hooks/useCategories.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export default function useCategories() {
+    const [categories, setCategories] = useState<string[]>([]);
+
+    // Just load a list of categories from a JSON file
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+    // Gets all categories
+    const fetchCategories = async () => {
+        const response = await fetch("/data/categories.json");
+        const json = await response.json();
+
+        const categories = json.map((category: any) => {
+            return category.name;
+        });
+
+        console.log(categories);
+
+        setCategories(categories);
+    };
+
+    return { categories };
+}
