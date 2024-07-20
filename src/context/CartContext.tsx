f2066323bc0d5ba7d6ce4a8191b02fcd851e35f4
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-07-20 19:30:15 -0300

    Add local storage functionality to CartProvider

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,7 +1,6 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { Product } from '../types/products';
 
-
 type CartContextType = {
     products: Product[];
     editQuantity: (product: Product, changes: number) => void;
@@ -18,15 +17,24 @@ type CartProviderProps = {
 
 export const CartProvider = ({ children }: CartProviderProps) => {
     const [products, setProducts] = useState<Product[]>([]);
+    const [firstLoad, setFirstLoad] = useState(true);
 
     useEffect(() => {
+        getCartFromLocalStorage();
+    }, []);
+
+    useEffect(() => {
+        if (firstLoad) {
+            return setFirstLoad(false);
+        }
+        
         const productsList = products;
         productsList.sort((a, b) => {
             if (a.code < b.code) return -1;
             if (a.code > b.code) return 1;
             return 0;
         });
-
+        saveCartToLocalStorage(productsList);
         setProducts(productsList);
     }, [products.length]);
 
@@ -44,10 +52,34 @@ export const CartProvider = ({ children }: CartProviderProps) => {
             return setProducts(products.filter((p) => p.code !== product.code));
         }
 
-        const newProducts = [...products]
+        const newProducts = [...products];
         newProducts[productIndex] = productCopy;
 
-        return setProducts(newProducts);
+        setProducts(newProducts);
+        saveCartToLocalStorage(newProducts); 
+    };
+
+    const getCartFromLocalStorage = () => {
+        const cart = localStorage.getItem('cart') || null;
+        if (!cart) {
+            return setProducts([]);
+        }
+        const parsedCart = JSON.parse(cart!);
+        if (!parsedCart?.products || !parsedCart?.createdAt) {
+            return setProducts([]);
+        }
+        // if cart is older than 24 hours, clear it
+        if (new Date(parsedCart.createdAt).getTime() + 24 * 60 * 60 * 1000 < new Date().getTime()) {
+            return setProducts([]);
+        }
+        else {
+            return setProducts(parsedCart.products);
+        }
+    };
+
+    const saveCartToLocalStorage = (newProducts: Product[]) => {
+        console.log('newProducts', newProducts);
+        localStorage.setItem('cart', JSON.stringify({products: newProducts, createdAt: new Date()}));
     };
 
     return (
