caad82101b3bc8ed5eca2aa3f3b4e559184fe476
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-03-10 02:01:17 -0300

    Update gatsby-config.ts with site metadata and additional plugins

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,26 +2,46 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `siete-colores-web`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Siete Colores`,
+    description: `Siete Colores es una empresa de impresión 3D y láser que se dedica a la fabricación de productos para artesanos.`,
+    siteUrl: `https://www.sietecolores3d.com.ar`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-transformer-json",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+        name: "Siete Colores",
+        short_name: "SieteColores3d",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        display: "standalone",
+      },
     },
-    __key: "images"
-  }]
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "data",
+        path: "./src/data/",
+      },
+    },
+  ],
 };
 
 export default config;
