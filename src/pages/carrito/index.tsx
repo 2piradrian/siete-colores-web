9d9f1d02fd8e5ed3df8f7bb6c5448b5d7169e37b
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-03-11 23:09:44 -0300

    Add container styles to carrito page and footer; enhance layout with padding and width adjustments

--- src/pages/carrito/index.tsx
+++ src/pages/carrito/index.tsx
@@ -4,13 +4,14 @@ import useViewModel from "../../ui/viewmodels/carrito/useViewModel";
 import CartPrices from "../../ui/components/cart-prices/cart-prices";
 import CartButtons from "../../ui/components/cart-buttons/cart-buttons";
 import { SEO } from "../../ui/components/seo/seo";
+import * as style from "./style.module.css";
 
 export default function CarritoPage(){
 
     const { products, editQuantity, subtotal, getOrder } = useViewModel();
 
     return (
-        <section>
+        <section className={style.container}>
             <CartList products={products} editQuantity={editQuantity}/>
             <CartPrices subtotal={subtotal} />
             <CartButtons products={products} getOrder={getOrder} />
